2703c9166fdc72c916852f7d318117ef8054f2cd
Fahd <fahds2005@icloud.com>
2025-08-23 15:27:16 -0700

    sliding-window: longest substring no duplicates (review)

--- sliding-window/longest-substring-no-duplicates.py
+++ sliding-window/longest-substring-no-duplicates.py
@@ -1,28 +1,40 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         """
-        understand:
-            set: b     
-            input: "abcabcbb"
-                           i
-                           j
-            out: 3
+            understanding:
+                given a string find of n characters
+                return the length of longest substring with repeating charactrers
 
-        approach:
-            1. iterate through a string and every char that unique to a set
-            2. while I'm i'll check while the current character is in the set if it is the increment the left index
-            3. I'll have a variable that will keep max_length every iteration I'll reset it by setting it equal to the max between it self and length of the charSet
+                example:
+                    input: "abcabcbb"
+                    output: 3, why? because the longest substring with repeating characters is 'abc'
+
+            approach:
+                  set: c, a, b
+                  max = 0, 1, 2, 3
+                  "abcabcbb"
+                     i 
+                        j
+
+
+            
         """
 
+        if len(s) == 0:
+            return 0
+
         charSet = set()
+        i = 0
         j = 0
-        max_length = 0
-        for i in range(len(s)):
+        max_substring = float('-inf')
+        while i < len(s):
             while s[i] in charSet:
                 charSet.remove(s[j])
                 j += 1
+
             charSet.add(s[i])
-            max_length = max(len(charSet), max_length)
+            max_substring = max(len(charSet), max_substring)
+            i += 1
 
+        return max_substring
 
-        return max_length
