d75c267f1290390b25939bb2162cdf24fd0e1979
Fahd <fahds2005@icloud.com>
2025-08-24 14:13:48 -0700

    slidering window: Longest Repeating char replacement (review)

--- sliding-window/Longest Repeating Character Replacement.py
+++ sliding-window/Longest Repeating Character Replacement.py
@@ -1,42 +1,55 @@
-
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         """
-            understand: 
-            i'm given a string s i can change the character of the string k amount of times, 
-            i have to return the longest substring containing the same characters after changing them
+        input: s: string, k: integer
+        output: the longest substring with k letter replacement
 
-            approach:
-                count: 
-                {
-                    A: 2
-                    B: 3
-                }
-                res: 0, 1, 2, 3, 4
-                current: AABABBA
-                           l
-                                r
+        requirments:
+            1. choose any character to change
+            2. you may only change a character k times
+        
+        approach:
+            "ABAB"
+            k: 2
+            {
+                a: 2
+                b: 2
+            }
+            max = 1, 2, 3, 4, 
+            AABABBA
+               l
+                  r
+
+            {
+                a: 3
+                b: 1
+            }
+            "AABABBA"
+             r
+                l
+
+            Time: O(n)
+            Space: O(n)
         """
 
-        max_length = 0
-        charCount = {}
         l = 0
-        maxf = 0
+        window = {}
+        result = float('-inf')
+
+        
+        max_occured_c = 0
         for r in range(len(s)):
-            window_length = (r - l + 1)
-            c = s[r]
-            charCount[c] = charCount.get(c, 0) + 1
-            maxf = max(maxf, charCount[c])
-            replacements = window_length - maxf
+            window[s[r]] = window.get(s[r], 0) + 1
+            max_occured_c = max(max_occured_c, window[s[r]])
 
-            while (r - l + 1) - maxf > k:
-                charCount[s[l]] -= 1
+            while (r - l + 1) - max_occured_c > k:
+                window[s[l]] -= 1
                 l += 1
-                maxf = max(maxf, charCount[c])
+            
+            result = max(result, (r - l + 1))
 
-            max_length = max(max_length, (r - l + 1))
+        return result
 
 
 
-        return max_length
         
