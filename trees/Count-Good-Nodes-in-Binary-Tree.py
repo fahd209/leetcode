73b1a85d83a013a479ecddb26173b606f20e2399
Fahd <fahds2005@icloud.com>
2025-09-15 16:42:34 -0700

    trees: count good nodes in a tree

--- trees/Count-Good-Nodes-in-Binary-Tree.py
+++ trees/Count-Good-Nodes-in-Binary-Tree.py
@@ -5,35 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
+    def goodNodes(self, root: TreeNode, max_parent=None) -> int:
         """
-        understanding:
-            given a tree count all the childern nodes
-            that are greater then or equal to its parent
+            given a binary tree count all the good nodes
+            a good node is a node in the tree that does not have a parent from the root to the cur node
+            that's greater then the current node
 
-        approach:
-            1. dfs while keeping track of the parent
-            2. if the cur node is greater then or equal to the parent we can increment the count by one
+            approach:
+                keep tree of the parent in a dfs
+                if there is cur that is not less then a previous parent then 
+                the new parent will cur for the childern
         """
 
-        return self.dfs(root, root)
-
-    def dfs(self, node, parent):
-
-        if node == None:
+        if root == None:
             return 0
-        
-        left = None
-        right = None
-        
-        if node.val >= parent.val:
-            left = self.dfs(node.left, node)
-        else:
-            left = self.dfs(node.left, parent)
 
-        if node.val >= parent.val:
-            right = self.dfs(node.right, node)
-        else:
-            right = self.dfs(node.right, parent)
+        if max_parent == None:
+            max_parent = root
 
-        return left + right + 1 if node.val >= parent.val else left + right
+        if root.val > max_parent.val:
+            max_parent = root 
+
+        left = self.goodNodes(root.left, max_parent)
+        right = self.goodNodes(root.right, max_parent)
+
+        return left + right + 1 if root.val >= max_parent.val else left + right
+        
