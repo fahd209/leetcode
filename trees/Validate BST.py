b937b26849987920869b114afd21fc98d3c27498
Fahd <fahds2005@icloud.com>
2025-10-18 14:52:15 -0700

    Trees: validate BST

--- trees/Validate BST.py
+++ trees/Validate BST.py
@@ -31,3 +31,54 @@ class Solution:
                 queue.append((cur.right, cur.val, right))
 
         return True
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Time: O(n)
+# Space: O(H) H = height of the tree
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        """
+        understanding: 
+            given a tree return true if the tree is BST else return False
+
+            rules:
+                every nodes left childern have to be strictly less then the node
+                every nodes right childern have tobe strictly greater then the node
+
+        approach:
+            1. DFS while checking the range or every node
+            2. Range: the root node can be anything from negative inf or postive inf
+               the left childern of that node can be anything from negative inf to any number that 
+               not equal to or greater then its parent. Every node to the right of the root
+               has to be between the value of its parent up to postive inf or the max number in the subtree.
+
+            3. if that condition false then we can stop iterating and return False
+
+        Complexsity:
+            Time: O(n)
+            Space: O(n)
+        """
+
+
+        def dfs(cur, left_range, right_range):
+
+            # return true because we got to a leaf node with out its parents failing
+            # the condition
+            if cur == None:
+                return True
+
+            if not left_range < cur.val < right_range:
+                return False 
+            
+            left = dfs(cur.left, left_range, cur.val)
+            right = dfs(cur.right, cur.val, right_range)
+
+            return left and right
+        return dfs(root, float('-inf'), float('inf'))
